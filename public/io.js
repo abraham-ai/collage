4b3ae80ef4bc39c9f52a9928877d044f80b5fe39
Gene Kogan <kogan.gene@gmail.com>
2022-10-04 02:04:26 +0000

    change gateway url

--- public/io.js
+++ public/io.js
@@ -14,7 +14,7 @@ var taskIds = [];
 var patchesLookup = {};
 var authToken = null;
 
-const GATEWAY_URL = "https://app.dev.aws.abraham.fun"
+const GATEWAY_URL = "https://gateway-test.abraham.ai"  //"https://app.dev.aws.abraham.fun"
 const MINIO_URL = "https://minio.aws.abraham.fun"
 const MINIO_BUCKET = "creations-stg"
 
